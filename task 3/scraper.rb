530ef36a7356097b2cb784135afe25f83cf6fcb5
Harshavardhan M <52796014+hvm2k1@users.noreply.github.com>
2019-09-05 18:11:17 +0530

    Update scraper.rb

--- task 3/scraper.rb
+++ task 3/scraper.rb
@@ -1,5 +1,26 @@
-require ‘nokogiri’
-require ‘open-uri’
-require ‘pry’
+require 'nokogiri'
+require 'open-uri'
 
-doc = Nokogiri::HTML(open(‘https://www.google.com/search?q=linux&ie=&oe='))
+class Scraper
+   puts "Enter word that you want to search. "
+   search = gets
+   doc = Nokogiri::HTML(open("https://www.google.com/search?q=#{search}&ie=&oe="))
+   a = []
+   link = []
+   heading = []
+   doc.xpath("//a/div").each do |link|
+      a.push(link.text)
+   end
+   for i in 0..a.length
+      if a[i] != nil AND a[i].include? "https://"
+            link.push(a[i])
+            heading.push(a[i-1])
+      end
+   end
+   for j in 0..link.length
+      puts heading[j]
+      puts link[j]
+      puts " "
+   end
+end
+Scraper
